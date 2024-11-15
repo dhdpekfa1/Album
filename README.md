df39f2633fe99f583ddaf1b1e74d4196a9e501d7
닮 <dhdpekfa1@daum.net>
2024-11-15 11:33:07 +0900

    [feat] 라우터 설정 및 기본 스타일 설정

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
+### 프로젝트 환경 설정
+
 1. 리액트 라우터 설치: `npm install react-router-dom`
+2. sass 설치: `npm install -D sass-embedded`
--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/Home";
+import BookmarkPage from "./pages/Bookmark";
+
 function App() {
   return (
-    <>
-      <h1>Init</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route index path="/" element={<HomePage />} />
+        <Route path="/bookmark" element={<BookmarkPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
