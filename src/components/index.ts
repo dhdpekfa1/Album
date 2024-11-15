02dc8d41a744d381d7a7240d820fe41b98caeb4e
닮 <dhdpekfa1@daum.net>
2024-11-15 10:59:43 +0900

    [init] 초기 설정

---
+++ src/components/index.ts
@@ -0,0 +1,34 @@
+export {
+  Card,
+  CardHeader,
+  CardFooter,
+  CardTitle,
+  CardDescription,
+  CardContent,
+} from "@/components/ui/card";
+export {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/components/ui/pagination";
+export {
+  Dialog,
+  DialogPortal,
+  DialogOverlay,
+  DialogClose,
+  DialogTrigger,
+  DialogContent,
+  DialogHeader,
+  DialogFooter,
+  DialogTitle,
+  DialogDescription,
+} from "@/components/ui/dialog";
+export { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
+export { Button, buttonVariants } from "@/components/ui/button";
+export { Input } from "@/components/ui/input";
+export { Separator } from "@/components/ui/separator";
+export { Skeleton } from "@/components/ui/skeleton";
